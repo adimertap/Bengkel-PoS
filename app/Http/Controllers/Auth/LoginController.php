ea660ae506fc08fa2eed6473c833473aa4363220
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-11 02:44:57 +0800

    fix sso

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-        return redirect()->to('/sso');
+        return redirect()->to('/pos');
     }
 
     public function username()
