bbdca5a0e6a651bf35ad8fb919a0574ee2c44169
adimertaaa <adimerta@student.unud.ac.id>
2022-01-14 00:20:59 +0800

    gg

--- resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
+++ resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
@@ -74,7 +74,7 @@
                                         <div class="font-weight-bold">{{ $item->pivot->jumlah }}</div>
                                     </td>
                                     <td colspan="10">
-                                        <div class="font-weight-bold">{{ $item->jenissparepart->masterdiskon->jumlah_diskon }}</div>
+                                        <div class="font-weight-bold">{{ $item->jenissparepart->diskon->masterdiskon->jumlah_diskon }}</div>
                                     </td>
                                  
                                     
