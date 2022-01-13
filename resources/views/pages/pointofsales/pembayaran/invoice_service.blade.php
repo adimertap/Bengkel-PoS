0b4b2b9c827358eed7165552df48e351ca62f701
adimertaaa <adimerta@student.unud.ac.id>
2022-01-13 23:17:08 +0800

    gg

--- resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
+++ resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
@@ -73,15 +73,13 @@
                                     <td colspan="10">
                                         <div class="font-weight-bold">{{ $item->pivot->jumlah }}</div>
                                     </td>
-                                    @if ($item->Jenissparepart->Diskon->Diskon == '' | $item->Jenissparepart->Diskon->Diskon == null)
-                                    <td colspan="10">
-                                        <div class="font-weight-bold">-</div>
-                                    </td>
-                                    @else
+                                 
+                                 
+                                    
                                     <td colspan="10">
                                         <div class="font-weight-bold">{{ $item->Jenissparepart->Diskon->Diskon->jumlah_diskon }}</div>
                                     </td>
-                                    @endif
+                                  
                                    
                                     <td class="text-right font-weight-bold">Rp.
                                         {{ number_format($item->pivot->total_harga,0,',','.') }}</td>
