7d22e324ca47e5d8c65b59500e755df988403a21
adimertaaa <adimerta@student.unud.ac.id>
2022-01-14 00:51:00 +0800

    gg

--- app/Http/Controllers/PointOfSales/Pembayaran/PembayaranServiceController.php
+++ app/Http/Controllers/PointOfSales/Pembayaran/PembayaranServiceController.php
@@ -67,10 +67,9 @@ class PembayaranServiceController extends Controller
 
 
         $customer = CustomerBengkel::where('id_customer_bengkel','=',$pembayaran_service->customer_bengkel->id_customer_bengkel)->get();
+        
         $diskon = Diskon::where('status_diskon','=','Diskon Khusus')->get();
-       
-
-        $laporan_service = LaporanService::where('nominal_bayar', '>=', $diskon->min_order)->groupBy($customer)->get();
+        $laporan_service = LaporanService::where('nominal_bayar', '>', $diskon->min_order)->get();
 
         return $laporan_service;
         
