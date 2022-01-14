3f26edbe2bf946c1dcac66a09088b8abd4522bc2
adimertaaa <adimerta@student.unud.ac.id>
2022-01-14 10:23:04 +0800

    gg

--- resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
+++ resources/views/pages/pointofsales/pembayaran/invoice_service.blade.php
@@ -90,7 +90,7 @@
                                         <td colspan="10">
                                             <div class="font-weight-bold">{{ $item->jenissparepart->diskon[0]->masterdiskon->jumlah_diskon ?? '-' }}%</div>
                                         </td>
-                                        @if ($item->jenissparepart->diskon == '')
+                                        @if (count($item->jenissparepart->diskon) == 0)
                                             <td class="text-right font-weight-bold">Rp.{{ number_format($item->pivot->total_harga,0,',','.')}}</td>
                                         @else
                                             <td class="text-right font-weight-bold">Rp.{{ number_format($item->pivot->total_harga-$item->pivot->total_harga*$item->jenissparepart->diskon[0]->masterdiskon->jumlah_diskon/100,0,',','.')}}</td>
