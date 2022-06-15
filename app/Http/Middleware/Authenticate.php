c6db93d6e2d61fe136948b60db3886c605526537
Adim <adimertap@gmail.com>
2022-06-16 02:31:28 +0800

    gas

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,7 +16,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return 'https://sso.bengkel-kuy.com';
+            return 'https://sso.e-bengkelku.com';
         }
     }
 }
